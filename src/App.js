a6761c609a0f2e59b596b185ece5695acbe86904
Blake Steel <blakemsteel@gmail.com>
2021-01-21 13:43:11 -0500

    React Integration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
+import Game from './game/game';
 
-function App() {
-  return (
-    <div className='App'>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      game: new Game()
+    }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.setState({ game: this.state.game }), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        Floor: {this.state.game.floor}
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Game from './game/game';
 import * as serviceWorker from './serviceWorker';
 
-const game = new Game();
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
